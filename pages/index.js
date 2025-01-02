28f56444395ad959faafe28375afe4e36b4a851d
Bruno Gomes de Souza <souzagbruno10@gmail.com>
2025-01-02 19:43:10 -0300

    alteracao na home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from 'next/image'; // Importação do componente Image
+import Image from 'next/image'; // Importação do componente Imagem
 import styles from '../styles/Home.module.css';
 
 function Home() {
